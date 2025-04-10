5764a7f3bc3f2b3ad27a91fdd716492268fe6a6c
adrian.ferrer.gutierrez <adrian.ferrer.gutierrez@estudiantat.upc.edu>
2025-04-10 15:54:05 +0000

    avances

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,13 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
 
+
   has_many :issues
   has_many :comments
   has_many :assigned_issues, class_name: 'Issue', foreign_key: 'assignee_id'
   has_and_belongs_to_many :watched_issues, class_name: 'Issue', join_table: 'issue_watchers'
+  has_one_attached  :avatar
+
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
