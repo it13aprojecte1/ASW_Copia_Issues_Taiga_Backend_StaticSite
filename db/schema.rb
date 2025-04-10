80700cf494ae03fa80b0603b1ef283fe937c14e4
adrian.ferrer.gutierrez <adrian.ferrer.gutierrez@estudiantat.upc.edu>
2025-04-10 15:56:23 +0000

    avances

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_10_100450) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_10_155339) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -116,6 +116,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_10_100450) do
     t.string "provider"
     t.string "uid"
     t.string "name"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
