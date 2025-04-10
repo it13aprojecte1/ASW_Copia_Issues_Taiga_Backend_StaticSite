cc963aafe96af0ccc07fcaccd24c85f15333dd26
adrian.ferrer.gutierrez <adrian.ferrer.gutierrez@estudiantat.upc.edu>
2025-04-10 11:32:56 +0000

    pequeñas modificaciones

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,18 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//import "turbo-rails"
-//import "controllers"
+import "turbo-rails"
+import "controllers"
 
-//import "@hotwired/turbo-rails"
-import Rails from "@rails/ujs"
-Rails.start()
 
-import { Application } from "@hotwired/stimulus"
-
-const application = Application.start()
-
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
-
-export { application }
 
